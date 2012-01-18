e62a6f0ad13765141e2bc4b9d8408652573acee6
congmo <congmogit@gmail.com>
2012-01-18 16:20:16 +0800

    add latest post,recent comments,firendly link...

--- _posts/2012-01-16-testtesttest.markdown
+++ _posts/2012-01-16-testtesttest.markdown
@@ -1,5 +1,6 @@
 ---
 layout: post
+title: teesesest
 ---
 ddddddddddddddddddddddddddddd
 ddddddddddddddddddddddddddddd
--- javascripts/jquery.github.js
+++ javascripts/jquery.github.js
@@ -1,3 +1,3 @@
 jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+  jQuery.getJSON("http://congmo.disqus.com/recent_comments_widget.js?num_items=5&amp;hide_avatars=0&amp;avatar_size=32&amp;excerpt_length=20" + "?callback=?", callback);
 }
