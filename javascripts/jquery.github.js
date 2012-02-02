286afe5bfa9b7e7451818779050875010ae03c57
congmo <congmogit@gmail.com>
2012-02-02 12:34:06 +0800

    commit some deleted

--- javascripts/jquery.github.js
+++
@@ -1,3 +0,0 @@
-jQuery.githubUser = function(username, callback) {
-  jQuery.getJSON("http://congmo.disqus.com/recent_comments_widget.js?num_items=5&amp;hide_avatars=0&amp;avatar_size=32&amp;excerpt_length=20" + "?callback=?", callback);
-}
